93bcdf3126058eedd053218d2425d7b74d46a8bf
Melissa <ha@melissadata.com>
2023-10-18 12:12:31 -0700

    Commit MatchUp Object Global sample code in Python3

---
+++ MelissaMatchupObjectGlobalWindowsPython3.ps1
@@ -0,0 +1,211 @@
+# Name:    MelissaMatchUpObjectGlobalWindowsPython3
+# Purpose: Use the Melissa Updater to make the MelissaMatchUpObjectGlobalWindowsPython3 code usable
+
+
+######################### Parameters ##########################
+
+param($global ='""', $us = '""', $license = '', [switch]$quiet = $false )
+
+######################### Classes ##########################
+
+class FileConfig {
+  [string] $FileName;
+  [string] $ReleaseVersion;
+  [string] $OS;
+  [string] $Compiler;
+  [string] $Architecture;
+  [string] $Type;
+}
+
+
+######################### Config ###########################
+
+$RELEASE_VERSION = '2023.Q3'
+$ProductName = "GLOBAL_MU_DATA"
+
+# Uses the location of the .ps1 file 
+# Modify this if you want to use 
+$CurrentPath = $PSScriptRoot
+Set-Location $CurrentPath
+$ProjectPath = "$CurrentPath\MelissaMatchupObjectGlobalWindowsPython3"
+$BuildPath = "$ProjectPath"
+$DataPath = "$ProjectPath\Data"
+
+If (!(Test-Path $DataPath)) {
+  New-Item -Path $ProjectPath -Name 'Data' -ItemType "directory"
+}
+
+# If (!(Test-Path $ProjectPath\Build)) {
+# New-Item -Path $ProjectPath -Name 'Build' -ItemType "directory"
+# }
+
+
+$DLLs = @(
+  [FileConfig]@{
+    FileName       = "mdGlobalParse.dll";
+    ReleaseVersion = $RELEASE_VERSION;
+    OS             = "WINDOWS";
+    Compiler       = "DLL";
+    Architecture   = "64BIT";
+    Type           = "BINARY";
+  },
+  [FileConfig]@{
+    FileName       = "mdMatchUp.dll";
+    ReleaseVersion = $RELEASE_VERSION;
+    OS             = "WINDOWS";
+    Compiler       = "DLL";
+    Architecture   = "64BIT";
+    Type           = "BINARY";
+  }
+)
+
+$Wrapper = [FileConfig]@{
+  FileName       = "mdMatchup_pythoncode.py";
+  ReleaseVersion = $RELEASE_VERSION;
+  OS             = "ANY";
+  Compiler       = "PYTHON";
+  Architecture   = "ANY" ;
+  Type           = "INTERFACE"
+}
+
+######################## Functions #########################
+
+function DownloadDataFiles([string] $license) {
+  $DataProg = 0
+  Write-Host "=================================== MELISSA UPDATER =================================="
+  Write-Host "MELISSA UPDATER IS DOWNLOADING DATA FILE(S)..."
+
+  .\MelissaUpdater\MelissaUpdater.exe manifest -p $ProductName -r $RELEASE_VERSION -l $license -t $DataPath 
+  if ($? -eq $False ) {
+    Write-Host "`nCannot run Melissa Updater. Please check your license string!"
+    Exit
+  }     
+  Write-Host "Melissa Updater finished downloading data file(s)!"
+
+}
+
+function DownloadDLLs() {
+  Write-Host "MELISSA UPDATER IS DOWNLOADING DLL(s)..."
+  $DLLProg = 0
+  foreach ($DLL in $DLLs) {
+    Write-Progress -Activity "Downloading DLL(s)" -Status "$([math]::round($DLLProg / $DLLs.Count * 100, 2))% Complete:"  -PercentComplete ($DLLProg / $DLLs.Count * 100)
+
+    # Check for quiet mode
+    if ($quiet) {
+      .\MelissaUpdater\MelissaUpdater.exe file --filename $DLL.FileName --release_version $DLL.ReleaseVersion --license $LICENSE --os $DLL.OS --compiler $DLL.Compiler --architecture $DLL.Architecture --type $DLL.Type --target_directory $ProjectPath > $null
+      if (($?) -eq $False) {
+        Write-Host "`nCannot run Melissa Updater. Please check your license string!"
+        Exit
+      }
+    }
+    else {
+      .\MelissaUpdater\MelissaUpdater.exe file --filename $DLL.FileName --release_version $DLL.ReleaseVersion --license $LICENSE --os $DLL.OS --compiler $DLL.Compiler --architecture $DLL.Architecture --type $DLL.Type --target_directory $ProjectPath 
+      if (($?) -eq $False) {
+        Write-Host "`nCannot run Melissa Updater. Please check your license string!"
+        Exit
+      }
+    }
+    
+    Write-Host "Melissa Updater finished downloading " $DLL.FileName "!"
+    $DLLProg++
+  }
+}
+
+function DownloadWrapper() {
+  Write-Host "MELISSA UPDATER IS DOWNLOADING WRAPPER(S)..."
+
+  # Check for quiet mode
+  if ($quiet) {
+    .\MelissaUpdater\MelissaUpdater.exe file --filename $Wrapper.FileName --release_version $Wrapper.ReleaseVersion --license $LICENSE --os $Wrapper.OS --compiler $Wrapper.Compiler --architecture $Wrapper.Architecture --type $Wrapper.Type --target_directory $ProjectPath > $null
+    if (($?) -eq $False) {
+      Write-Host "`nCannot run Melissa Updater. Please check your license string!"
+      Exit
+    }
+  }
+  else {
+    .\MelissaUpdater\MelissaUpdater.exe file --filename $Wrapper.FileName --release_version $Wrapper.ReleaseVersion --license $LICENSE --os $Wrapper.OS --compiler $Wrapper.Compiler --architecture $Wrapper.Architecture --type $Wrapper.Type --target_directory $ProjectPath 
+    if (($?) -eq $False) {
+      Write-Host "`nCannot run Melissa Updater. Please check your license string!"
+      Exit
+    }
+  }
+
+  Write-Host "Melissa Updater finished downloading " $Wrapper.FileName "!"
+}
+
+
+function CheckDLLs() {
+  Write-Host "`nDouble checking dll(s) were downloaded...`n"
+  $FileMissing = $false 
+  if (!(Test-Path ("$BuildPath\mdMatchup.dll"))) {
+    Write-Host "mdMatchup.dll not found." 
+    $FileMissing = $true
+  }
+  if (!(Test-Path ("$BuildPath\mdGlobalParse.dll"))) {
+    Write-Host "mdGlobalParse.dll not found." 
+    $FileMissing = $true
+  }
+  if ($FileMissing) {
+    Write-Host "`nMissing the above data file(s).  Please check that your license string and directory are correct."
+    return $false
+  }
+  else {
+    return $true
+  }
+}
+
+
+
+########################## Main ############################
+
+Write-Host "`n============================ Melissa MatchUp Object Global ===========================`n                             [ Python3 | Windows | 64BIT ]`n"
+
+# Get license (either from parameters or user input)
+if ([string]::IsNullOrEmpty($license) ) {
+  $License = Read-Host "Please enter your license string"
+}
+
+# Check for License from Environment Variables 
+if ([string]::IsNullOrEmpty($License) ) {
+  $License = $env:MD_LICENSE # Get-ChildItem -Path Env:\MD_LICENSE   #[System.Environment]::GetEnvironmentVariable('MD_LICENSE')
+}
+
+if ([string]::IsNullOrEmpty($License)) {
+  Write-Host "`nLicense String is invalid!"
+  Exit
+}
+# Use Melissa Updater to download data file(s) 
+# Download data file(s) 
+DownloadDataFiles -license $License      # comment out this line if using Release
+
+# Set data file(s) path
+#$DataPath = "C:\Program Files\Melissa DATA\DQT\Data"      # uncomment this line and change to your Release data file(s) directory 
+
+# Download dll(s)
+DownloadDlls -license $License
+
+# Download wrapper(s)
+DownloadWrapper -license $License
+
+# Check if all dll(s) have been downloaded. Exit script if missing
+$DLLsAreDownloaded = CheckDLLs
+if (!$DLLsAreDownloaded) {
+  Write-Host "`nAborting program, see above.  Press any button to exit."
+  $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+  exit
+}
+
+Write-Host "All file(s) have been downloaded/updated! "
+
+# Start
+# Run project
+if ([string]::IsNullOrEmpty($global) -and [string]::IsNullOrEmpty($us)) {
+  Push-Location MelissaMatchupObjectGlobalWindowsPython3
+  python3 MelissaMatchupObjectGlobalWindowsPython3.py --license $License  --dataPath $DataPath
+  Pop-Location
+}
+else {
+  Push-Location MelissaMatchupObjectGlobalWindowsPython3
+  python3 MelissaMatchupObjectGlobalWindowsPython3.py --license $License  --dataPath $DataPath --global $global --us $us
+  Pop-Location
+}
--- README.md
+++ README.md
@@ -1,3 +1,157 @@
-# Melissa - MatchUp Object Global Windows Python3 
+# Melissa - MatchUp Object Global Windows Python3
 
-Coming soon
+## Purpose
+
+This code showcases the Melissa MatchUp Object Global using Python3.
+
+Please feel free to copy or embed this code to your own project. Happy coding!
+
+For the latest Melissa MatchUp Object Global release notes, please visit: https://releasenotes.melissa.com/on-premise-api/matchup-object-global/
+
+For further details, please visit: https://wiki.melissadata.com/index.php?title=MatchUp_Object:Reference
+
+The console will ask the user for:
+
+- A txt file that contains global data you would like to matchup
+- A txt file that contains US data you would like to matchup
+
+And return 
+
+- A txt file of global duplicate groups
+- A txt file of US duplicate groups
+
+## Tested Environments
+
+- Windows 64-bit Python 3.8.7
+- Powershell 5.1
+- Melissa data files for 2023-Q3
+
+## Required File(s) and Programs
+
+#### Binaries
+This is the c++ code of the Melissa Object.
+
+- mdMatchup.dll
+- mdGlobalParse.dll
+
+#### Data File(s)
+- icudt52l.dat
+- mdMatchup.dat
+- mdMatchup.mc
+- mdMatchup.sac
+- mdMatchup.cfg
+
+
+## Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+This project is compatible with Python3
+
+#### Install Python3
+Before starting, make sure that Python3 has been correctly installed on your machine and your environment paths are configured. 
+
+You can download Python here: 
+https://www.python.org/downloads/
+
+To set up your Path to correctly to use the python3 command, execute the following steps:
+1) Run Powershell as an administrator 
+2) Execute the command: 
+`New-Item -ItemType SymbolicLink -Path "Link" -Target "Target"`
+
+    where "Target" is the path to py.exe (by default this should be "C:\Windows\py.exe")\
+    and "Link" is the path to py.exe, but "py.exe" is replaced with "python3.exe"\
+    For Example:\
+    `New-Item -ItemType SymbolicLink -Path "C:\Windows\python3.exe" -Target "C:\Windows\py.exe"`
+
+If you are unsure, you can check by opening a command prompt window and typing the following:
+`python3 --version`
+
+![alt text](/screenshots/python_version.PNG)
+
+If you see the version number then you have installed Python3 and set up your environment paths correctly!
+
+----------------------------------------
+
+#### Set up Powershell settings
+
+If running Powershell for the first time, you will need to run this command in the Powershell console: `Set-ExecutionPolicy RemoteSigned`.
+The console will then prompt you with the following warning shown in the image below. 
+ - Enter `'A'`. 
+ 	- If successful, the console will not output any messages. (You may need to run Powershell as administrator to enforce this setting).
+	
+ ![alt text](/screenshots/powershell_executionpolicy.png)
+
+----------------------------------------
+
+#### Download this project
+```
+$ git clone https://github.com/MelissaData/MatchUpObjectGlobal-Python3
+$ cd MatchUpObjectGlobal-Python3
+```
+
+#### Set up Melissa Updater 
+
+Melissa Updater is a CLI application allowing the user to update their Melissa applications/data. 
+
+- Download Melissa Updater here: <https://releases.melissadata.net/Download/Library/WINDOWS/NET/ANY/latest/MelissaUpdater.exe>
+- Create a folder within the cloned repo called `MelissaUpdater`.
+- Put `MelissaUpdater.exe` in `MelissaUpdater` folder you just created.
+
+----------------------------------------
+
+#### Different ways to get data file(s)
+1.  Using Melissa Updater
+	- It will handle all of the data download/path and dll(s) for you. 
+2.  If you already have the latest zip, you can find the data file(s) and dll(s) in there
+	- Use the location of where you copied/installed the data and update the "$DataPath" variable in the powershell script.
+	- Copy all the dll(s) mentioned above into the `MelissaMatchUpObjectGlobalWindowsPython3` project folder.
+	
+----------------------------------------
+
+## Run Powershell Script
+Parameters:
+- -global: a test global txt file
+- -us: a test US txt file
+
+  These are convenient when you want to get results for specific txt files in one run instead of testing multiple txt files in interactive mode.
+
+- -license (optional): a license string to test the MatchUp Object Global
+- -quiet (optional): add to the command if you do not want to get any console output from the Melissa Updater
+
+
+When you have modified the script to match your data location, let's run the script. There are two modes:
+- Interactive 
+
+	The script will prompt the user for a txt file, then use the provided txt file to test MatchUp Object Global.  For example:
+    ```
+    $ .\MelissaMatchupObjectGlobalWindowsPython3.ps1
+    ```
+    For quiet mode:
+    ```
+    $ .\MelissaMatchupObjectGlobalWindowsPython3.ps1 -quiet
+    ```
+- Command Line 
+
+    You can pass a global txt file, us txt file, and a license string into the ```-global```, ```-us```, and ```-license``` parameters respectively to test MatchUp Object Global. For example:
+    ```
+    $ .\MelissaMatchupObjectGlobalWindowsPython3.ps1 -global "MelissaMatchupGlobalSampleInput.txt" -us "MelissaMatchupUSSampleInput.txt"
+    $ .\MelissaMatchupObjectGlobalWindowsPython3.ps1 -global "MelissaMatchupGlobalSampleInput.txt" -us "MelissaMatchupUSSampleInput.txt" -license "<your_license_string>"
+    ```
+    For quiet mode:
+    ```
+    $ .\MelissaMatchupObjectGlobalWindowsPython3.ps1 -global "MelissaMatchupGlobalSampleInput.txt" -us "MelissaMatchupUSSampleInput.txt" -quiet
+    $ .\MelissaMatchupObjectGlobalWindowsPython3.ps1 -global "MelissaMatchupGlobalSampleInput.txt" -us "MelissaMatchupUSSampleInput.txt" -license "<your_license_string>" -quiet
+    ```
+This is the expected output from a successful setup for interactive mode:
+
+
+![alt text](/screenshots/output.png)
+
+
+## Contact Us
+
+For free technical support, please call us at 800-MELISSA ext. 4
+(800-635-4772 ext. 4) or email us at tech@melissa.com.
+
+To purchase this product, contact Melissa sales department at
+800-MELISSA ext. 3 (800-635-4772 ext. 3).
