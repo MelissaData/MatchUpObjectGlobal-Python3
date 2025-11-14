37111556ef70cf29712eff2a32afb063a86d9d9f
Melissa <>
2025-11-14 11:00:04 -0800

    Tested successfully using Melissa data files for 2025.Q4

--- MelissaMatchupObjectGlobalWindowsPython3.ps1
+++ MelissaMatchupObjectGlobalWindowsPython3.ps1
@@ -19,7 +19,7 @@ class FileConfig {
 
 ######################### Config ###########################
 
-$RELEASE_VERSION = '2025.Q3'
+$RELEASE_VERSION = '2025.Q4'
 $ProductName = "GLOBAL_MU_DATA"
 
 # Uses the location of the .ps1 file 
